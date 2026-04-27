7f38a895857c99e023a3953355608f3fa7adfb60
Vasuppradha <vasuppradha0310@gmail.com>
2026-04-27 23:19:41 +0530

    Add files via upload

---
+++ Dior/index.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.nav-items a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      const href = this.getAttribute('href');
+      if (href.startsWith('#')) {
+        e.preventDefault();
+        const target = document.querySelector(href);
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  });
